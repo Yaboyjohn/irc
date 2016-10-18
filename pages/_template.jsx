38ccd153e061fb503f6394e2ecdc6b2f183aa6a2
Peter Qiu <yingchong.qiu@berkeley.edu>
2016-10-17 19:25:54 -0700

    basic navbar  (#4)
    * navbar first
    
    * navbar first
    
    * changes for pull request
    
    * remove line and span

--- pages/_template.jsx
+++ pages/_template.jsx
@@ -4,43 +4,49 @@ import { Link } from 'react-router';
 import { prefixLink } from 'gatsby-helpers';
 
 require('css/_index.scss');
-
 export default class Template extends Component {
   render () {
     return (
       <div>
         <Headroom>
-          <Link
-            to={prefixLink('/')}
-          >
-            Home
-          </Link>
-
-          <Link
-            to={prefixLink('/about/')}
-          >
-            About
-          </Link>
-
-          <Link
-            to={prefixLink('/events/')}
-          >
-            Events
-          </Link>
-
-          <Link
-            to={prefixLink('/review/')}
-          >
-            Review
-          </Link>
-
-          <Link
-            to={prefixLink('/contact/')}
-          >
-            Contact
-          </Link>
+          <div className="navbar">
+            <div className="navbar__link--wrapper">
+              <Link
+                to={prefixLink('/')} className="navbar__link"
+              >
+                Home
+              </Link>
+            </div>
+            <div className="navbar__link--wrapper">
+              <Link
+                to={prefixLink('/about/')} className="navbar__link"
+              >
+                About
+              </Link>
+            </div>
+            <div className="navbar__link--wrapper">
+              <Link
+                to={prefixLink('/events/')} className="navbar__link"
+              >
+                Events
+              </Link>
+            </div>
+            <div className="navbar__link--wrapper">
+              <Link
+                to={prefixLink('/review/')} className="navbar__link"
+              >
+                Review
+              </Link>
+            </div>
+            <div className="navbar__link--wrapper">
+              <Link
+                to={prefixLink('/contact/')} className="navbar__link"
+              >
+                Contact
+              </Link>
+            </div>
+          </div>
         </Headroom>
-
         <div className="content">
           {this.props.children}
         </div>
